8df255fec8222710d0990404f12320d737101c14
paul <bitowaqr@gmail.com>
2021-02-20 21:43:46 +0100

    solution 5 fix and plot

--- Intro to R Shiny/Solutions/Solution_5.R
+++ Intro to R Shiny/Solutions/Solution_5.R
@@ -8,8 +8,8 @@
 # install.packages("shiny") # necessary if you don't already have the function 'shiny' installed.
 
 library(shiny)   # we need the function shiny installed, this loads it from the library.      
+library(shinythemes)
 
-# 1. source the scripts that contain the model functions from the Markov Model/src folder.
 source("Markov Model/src/f_gen_psa.R")
 source("Markov Model/src/f_MM_sicksicker.R")
 source("Markov Model/src/f_wrapper.R")
@@ -18,29 +18,32 @@ source("Markov Model/src/f_wrapper.R")
 #                   Create User Interface
 #================================================================
 
-ui <- fluidPage(    # create user interface using fluid-page function
+ui <- fluidPage(    # create user interface using fluidpage function
+  theme = shinytheme("sandstone"),    # set a theme for your app
+  # see https://rstudio.github.io/shinythemes/ for more options                       
+  titlePanel("Sicker sicker Model in Shiny"),   # title of app
   
-  titlePanel("Sick Sicker Model in Shiny"),   # title of app
+  
   
   # SIDEBAR
   sidebarLayout(    # indicates layout is going to be a sidebar-layout
     
     sidebarPanel( # open sidebar panel
       
-      numericInput(inputId = "SI_c_Trt",      # id of input, used in server
-                   label = "Treatment Cost",  # label next to numeric input
+      numericInput(inputId = "x",           # id of input, used in server
+                   label = "Treatment Costs", # label next to numeric input
                    value = 200,               # initial value
+                   min = 0,                   # minimum value allowed
+                   max = 100000),              # maximum value allowed
+      
+      numericInput(inputId = "y",      # id of input, used in server
+                   label = "PSA runs",        # label next to numeric input
+                   value = 200,              # initial value
                    min = 0,                   # minimum value allowed
                    max = 400),                # maximum value allowed
       
-      numericInput(inputId = "SI_n_sim",      # id of input, used in server
-                   label = "PSA runs",        # label next to numeric input
-                   value = 1000,              # initial value
-                   min = 0,                   # minimum value allowed
-                   max = 2000),                # maximum value allowed
-      
-      sliderInput(inputId = "SI_n_age_init",  # id of input, used in server
-                  label = "Initial Age",      # label next to numeric input
+      sliderInput(inputId = "z",  # id of input, used in server
+                  label = "Initial age",      # label next to numeric input
                   value = 25,                 # initial value
                   min = 10,                   # minimum value allowed
                   max = 80),                  # maximum value allowed
@@ -49,17 +52,20 @@ ui <- fluidPage(    # create user interface using fluid-page function
       actionButton(inputId = "run_model",     # id of action button, used in server
                    label   = "Run model")     # action button label (on button)
       
+      
     ),  # close sidebarPanel
     
-    mainPanel(                                # open main panel
-      # 2. INSERT THE TEXT OUTPUT HERE
-      textOutput(outputId = "printICER")     # text output                
+    mainPanel(            # open main panel
+      
+      h3("Results") , 
+      
+      plotOutput(outputId = "graph")
       
     ) # close mainpanel    
     
   ) # close sidebarlayout
   
-) # close UI fluidpage
+) # close UI fluid page
 
 
 #================================================================
@@ -68,28 +74,26 @@ ui <- fluidPage(    # create user interface using fluid-page function
 
 server <- function(input, output){   # server = function with two inputs
   
-  observeEvent(input$run_model,       # when action button pressed ...
-               ignoreNULL = F, {
+  
+  
+  
+  observeEvent(input$run_model,{       # when action button pressed ...
                  
-                 # 3. Run model wrapper function (from within the run_model observe event function) with the Shiny inputs 
-                 #    and store the results as data-frame 
-                 df_model_res = f_wrapper(c_Trt = input$SI_c_Trt,
-                                          n_age_init = input$SI_n_age_init,
-                                          n_sim = input$SI_n_sim)
+                 model_res = f_wrapper(
+                   c_Trt = input$x,
+                   n_sim = input$y,
+                   n_age_init = input$z
+                   )
                  
-                 # 4. calculate the ICER from the PSA outputs.
-                 icer <- mean(df_model_res$Cost_Trt) - mean(df_model_res$Cost_NoTrt) /
-                         mean(df_model_res$QALY_Trt) - mean(df_model_res$QALY_NoTrt)
-                 
-                 # 5. Display the ICER using the renderText function
-                 output$printICER <- renderText({
+                 #--- CREATE GRAPH IN SERVER ---#
+                 output$graph <- renderPlot({
                    
-                   paste0("The ICER is: £", round(icer,2))
-                   
+                   plot(x = isolate(model_res$Cost_Trt-model_res$Cost_NoTrt), 
+                        y = isolate(model_res$QALY_Trt-model_res$QALY_NoTrt))
+
                  }) # render Text end.
                  
-               }) # Observe Event End
-  
+               }) # Observe Event End  
   
 } # Server end
 
